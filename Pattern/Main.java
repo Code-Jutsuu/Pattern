e88aa4e5a771270eb00f4dfb38f89936def1c186
Himanshu Verma <himanshuv309@gmail.com>
2023-10-03 20:01:35 +0530

    NEW

--- Pattern/Main.java
+++ Pattern/Main.java
@@ -1,9 +1,21 @@
-package Pattern;
+
 
 public class Main {
     public static void main(String[] args) {
         
+        int n =5;
         
-        System.out.println("gandu");
+        square(n);
+    }
+    static void square(int n){
+         
+        
+        for(int i =1 ; i<=n; i++){
+            for(int j = 1;j<=5; j++ ) {
+                System.out.print("8 ");
+            }
+             System.out.println();
+        }
     }
 }
+
