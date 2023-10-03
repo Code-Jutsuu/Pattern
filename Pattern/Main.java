d15dd45e4ab1de1745300d053c0de57a04c6afbf
Himanshu Verma <himanshuv309@gmail.com>
2023-10-03 13:49:07 +0530

    gg

--- Pattern/Main.java
+++ Pattern/Main.java
@@ -3,11 +3,7 @@ package Pattern;
 public class Main {
     public static void main(String[] args) {
         
-        for(int i=1; i<5; i++){
-            for(int j=1; j<5; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        
+        System.out.println("gandu");
     }
 }
